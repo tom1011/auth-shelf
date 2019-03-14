a33a0752e81e7a15a20697f7a91b42625f12304f
David Friday <davidsfriday13@gmail.com>
2019-03-14 14:33:31 -0500

    get saga/reducer set up for shelf

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,7 +6,13 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-    res.sendStatus(200); // For testing only, can be removed
+    const queryText = `SELECT "description", "image_url" FROM "item";`
+    pool.query(queryText)
+        .then((result) => { res.send(result.rows); })
+        .catch((err) => {
+            console.log('Error completing get shelf query', err);
+            res.sendStatus(500);
+        });
 });
 
 
